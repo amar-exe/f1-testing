3463cd59112b81ac0fff93a0eaa017435c49aeec
Amar Fazlić <amar.fazlic@stu.ibu.edu.ba>
2023-01-03 15:33:31 +0100

    Api call and Driver Model with standings

---
+++ src/main/java/ba/edu/ibu/ApiGetter.java
@@ -0,0 +1,47 @@
+package ba.edu.ibu;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class ApiGetter {
+
+
+    public static DriverModel getDrivers2022() {
+        DriverModel driverModel = null;
+        try {
+            URL url = new URL("http://ergast.com/api/f1/2022/driverStandings.json");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("POST");
+
+
+            con.setDoOutput(true);
+            DataOutputStream out = new DataOutputStream(con.getOutputStream());
+            out.flush();
+            out.close();
+
+            int responseCode = con.getResponseCode();
+            System.out.println("Response code: " + responseCode);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+
+            Gson gson = new Gson();
+            driverModel = gson.fromJson(String.valueOf(response.substring(10, response.length()-1)), DriverModel.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return driverModel;
+    }
+}
--- src/main/java/ba/edu/ibu/App.java
+++ src/main/java/ba/edu/ibu/App.java
@@ -2,6 +2,11 @@ package ba.edu.ibu;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    DriverModel driverModel = ApiGetter.getDrivers2022();
+    for (DriverModel.StandingsList standingsList : driverModel.StandingsTable.StandingsLists) {
+      for (DriverModel.DriverStandings driverStandings : standingsList.DriverStandings) {
+        System.out.println(driverStandings.Driver.givenName + " " + driverStandings.Driver.familyName + ": " + driverStandings.points + " points");
+      }
+    }
   }
 }
