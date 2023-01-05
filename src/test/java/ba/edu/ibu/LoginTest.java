2cd06409177495d098db5794db85277776fd4945
saba8814 <ejub1946@outlook.com>
2023-01-05 18:16:35 +0100

    hvala-Allahu-gospodaru-svih-svjetova-da-se-i-ovo-zavrsi

--- src/test/java/ba/edu/ibu/LoginTest.java
+++ src/test/java/ba/edu/ibu/LoginTest.java
@@ -1,12 +1,11 @@
 package ba.edu.ibu;
 
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -15,39 +14,36 @@ import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LoginTest {
-    private static WebDriver webDriver;
-
-    @BeforeAll
-    static void setUp() {
-        System.setProperty("webdriver.chrome.driver",
-                Config.path+"chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized");
-        webDriver = new ChromeDriver(options);
-    }
-
-    @AfterAll
-    static void tearDown() {
-        webDriver.quit();
-    }
 
     @Test
     void successfulLoginTest() throws IOException, InterruptedException {
-        //Runtime.getRuntime().exec(Config.path+"loginTest\\main.exe", null, new File(Config.path+"loginTest"));
-        //Thread.sleep(60000);
+        String myString = "@";
+        StringSelection stringSelection = new StringSelection(myString);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(stringSelection, null);
+
+        Runtime.getRuntime().exec(Config.path+"f1-testing\\loginTest\\main.exe", null, new File(Config.path+"f1-testing\\loginTest"));
         File file = new File(
-                Config.path+"loginTest\\output.txt");
+                Config.path+"f1-testing\\loginTest\\output.txt");
+        while(!file.exists()){
+            Thread.sleep(5000);
+            file=new File(
+                    Config.path+"f1-testing\\loginTest\\output.txt");
+        }
 
         BufferedReader br
                 = new BufferedReader(new FileReader(file));
 
-        String st;
-        while ((st = br.readLine()) != null)
-            if(st.equals("PASSED") || st.equals("FAILED")){
-                assertEquals("PASSED",st);
-                break;
-            }
+        String text = br.readLine();
+        br.close();
+        file.delete();
+        assertEquals("PASSED",text);
 
     }
-
+    @AfterAll
+    static void tearDown(){
+        File file = new File(
+                Config.path+"f1-testing\\loginTest\\output.txt");
+        file.delete();
+    }
 }
